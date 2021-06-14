4a5941570e849730c49a5504418c81110c6866cf
Giulio_Pa <giuliop93@hotmail.it>
2021-06-14 17:38:04 +0200

    fatal errors o.O

--- index.php
+++ index.php
@@ -1,33 +1,66 @@
 <?php
 
-class Movie {
+class Movie
+{
 
     public $title;
 
-    function __construct($title, $duration) {
+    function __construct($title, $duration)
+    {
 
         $this->title = $title;
         $this->duration = $duration;
     }
 
-    public function setAudience($eta) {
+    public function setAudience($eta)
+    {
         if ($eta < 18) {
             $this->audience = false;
         }
     }
 
-    public function getAudience() {
+    public function getAudience()
+    {
         return $this->audience;
     }
-    
 }
 
-$movie->title = "Francesco e la borraccia magica";
+// $movie->title = "Francesco e la borraccia magica";
 
-$memento = new Movie(120);
+$movie1 = new Movie("Francesco e la borraccia magica", 120);
+$movie2 = new Movie("Francesco e il gatto sulla tastiera", 125);
 
 $movie->setAudience();
 
 $movie_audience = $movie->getAudience()
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li>
+            <?php echo ($movie1) ?>
+        </li>
+        <li>
+            <?php echo ($movie2) ?>
+        </li>
+
+    </ul>
+
+
+
+
+</body>
+
+</html>
