0a6ff9c3ba983c92d4b9b0858fb28d21f8f3fec1
Giulio_Pa <giuliop93@hotmail.it>
2021-06-14 18:02:47 +0200

    wip

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 class Movie
 {
-
     public $title;
 
     function __construct($title, $duration)
@@ -11,6 +10,8 @@ class Movie
         $this->title = $title;
         $this->duration = $duration;
     }
+
+    public $audience;
 
     public function setAudience($eta)
     {
@@ -25,42 +26,33 @@ class Movie
     }
 }
 
-// $movie->title = "Francesco e la borraccia magica";
-
 $movie1 = new Movie("Francesco e la borraccia magica", 120);
+
 $movie2 = new Movie("Francesco e il gatto sulla tastiera", 125);
 
 $movie1->setAudience(20);
 
 $movie_audience = $movie1->getAudience(20);
-
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <ul>
         <li>
-            <?php echo ($movie1->title) ?>
+            <?php echo ($movie1->title)?>
         </li>
         <li>
-            <?php echo ($movie2->title) ?>
+            <?php echo ($movie2->title)?>
         </li>
-
     </ul>
-
-
-
-
 </body>
 
 </html>
